f45fe5fcf7a1d0de208e21cc38e0356c9f287fd3
Arul Agrawal <me@arul.io>
2022-05-03 14:25:27 +0200

    setup project using nx.dev currently using nestjs + angular preset

---
+++ apps/office-booker/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@office-booker/api-interfaces';
+
+@Component({
+  selector: 'office-booker-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}
