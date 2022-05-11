b0edfdd2f7e54161ffb2caf1ba481d88b10102af
Grant Bursnall <gbursnall@gmail.com>
2022-05-11 10:20:04 +0200

    ✨ (view-bookings): Added angular material and grid

---
+++ apps/office-booker/src/app/bookings/bookings.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookingsComponent } from './bookings.component';
+
+describe('BookingsComponent', () => {
+  let component: BookingsComponent;
+  let fixture: ComponentFixture<BookingsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BookingsComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ apps/office-booker/src/app/bookings/bookings.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'office-booker-bookings',
+  templateUrl: './bookings.component.html',
+  styleUrls: ['./bookings.component.css'],
+})
+export class BookingsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void { }
+}
