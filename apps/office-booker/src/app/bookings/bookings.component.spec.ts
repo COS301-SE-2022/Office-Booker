a09a8595856a4b08cd38e41a2ba601b927d58dfb
Grant Bursnall <gbursnall@gmail.com>
2022-05-11 22:51:03 +0200

    needed one test

--- apps/office-booker/src/app/bookings/bookings.component.spec.ts
+++ apps/office-booker/src/app/bookings/bookings.component.spec.ts
@@ -2,9 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookingsComponent } from './bookings.component';
 
-// describe('BookingsComponent', () => {
-//   let component: BookingsComponent;
-//   let fixture: ComponentFixture<BookingsComponent>;
+describe('BookingsComponent', () => {
+  let component: BookingsComponent;
+  let fixture: ComponentFixture<BookingsComponent>;
 
 //   beforeEach(async () => {
 //     await TestBed.configureTestingModule({
@@ -18,7 +18,7 @@ import { BookingsComponent } from './bookings.component';
 //     fixture.detectChanges();
 //   });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
