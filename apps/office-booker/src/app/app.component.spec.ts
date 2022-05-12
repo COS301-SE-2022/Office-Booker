c07761a9dacc5c8f402735841136de6a2f4cb089
Arul Agrawal <me@arul.io>
2022-05-12 16:38:30 +0200

    fix tests by making required modules and components available

--- apps/office-booker/src/app/app.component.spec.ts
+++ apps/office-booker/src/app/app.component.spec.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { BookingsComponent } from './bookings/bookings.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      imports: [HttpClientModule],
+      imports: [HttpClientModule, MatDialogModule, MatGridListModule],
+      declarations: [AppComponent, MenuBarComponent, BookingsComponent],
     }).compileComponents();
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
