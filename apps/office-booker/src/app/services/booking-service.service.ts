ca5bbe0b6742ad51b73e4a2e2cdc128d2e3af2f0
u20538945 <u20538945@tuks.co.za>
2022-05-12 15:16:44 +0200

    Service Functions calling API Requests

--- apps/office-booker/src/app/services/booking-service.service.ts
+++ apps/office-booker/src/app/services/booking-service.service.ts
@@ -12,4 +12,49 @@ export class BookingServiceService {
     const url = 'http://localhost:3333/api/rooms';
     return this.http.get(`${url}`);
   }
+  
+  getRoomByID(roomId: number){
+    const url = 'http://localhost:3333/api/rooms/' + roomId;
+    return this.http.get(`${url}`);
+  }
+
+  getDesksByRoomId( roomId:number) {
+    const url = 'http://localhost:3333/api/desks/room/' + roomId;
+    return this.http.get(`${url}`);
+  }
+
+  getAllDesks() {
+    const url = 'http://localhost:3333/api/desks';
+    return this.http.get(`${url}`);
+  }
+
+  getFacilitiesByDeskId(deskId: number){
+    const url = 'http://localhost:3333/api/facilities/desk/' + deskId;
+    return this.http.get(`${url}`);
+  }
+
+  getBookingsByDeskId(deskId: number){
+    const url = 'http://localhost:3333/api/bookings/desk/' + deskId;
+    return this.http.get(`${url}`);
+  }
+
+  getBookingByBookingId(bookingId: number){
+    const url = 'http://localhost:3333/api/bookings/' + bookingId;
+    return this.http.get(`${url}`);
+  }
+
+  getCurrentBooking(deskId: number){
+    const url = 'http://localhost:3333/api/bookings/desk/current/' + deskId;
+    return this.http.get(`${url}`);
+  }
+
+  /*deleteBooking(bookingId: number){
+    const url = 'http://localhost:3333/api/bookings/' + bookingId;
+    return this.http.delete(`${url}`);
+  }*/
+
+  /*setDesk(deskId: number){
+    const url = 'http://localhost:3333/api/desk/' + deskId;
+    return this.http.post(`${url}`, deskId);
+  }*/
 }
