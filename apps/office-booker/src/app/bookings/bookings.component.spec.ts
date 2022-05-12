330659ad269f44014e1f5818dee5b4a20b5594ef
Arul Agrawal <me@arul.io>
2022-05-12 12:49:01 +0200

    fix tsconfig jest type error and fix missing import for test

--- apps/office-booker/src/app/bookings/bookings.component.spec.ts
+++ apps/office-booker/src/app/bookings/bookings.component.spec.ts
@@ -1,22 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialogModule } from '@angular/material/dialog';
 import { BookingsComponent } from './bookings.component';
 
 describe('BookingsComponent', () => {
   let component: BookingsComponent;
   let fixture: ComponentFixture<BookingsComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [BookingsComponent],
-//     }).compileComponents();
-//   });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MatDialogModule],
+      declarations: [BookingsComponent],
+    }).compileComponents();
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(BookingsComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
