826f92eb16b7925c694ee05789e8a395fc38b8ad
Arul Agrawal <me@arul.io>
2022-05-12 16:38:28 +0200

    remove unused import

--- apps/office-booker/src/app/bookings/bookings.component.ts
+++ apps/office-booker/src/app/bookings/bookings.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { BookingDialogComponent } from '../booking-dialog/booking-dialog.component';
 import { BookingServiceService } from '../services/booking-service.service';
+
 @Component({
   selector: 'office-booker-bookings',
   templateUrl: './bookings.component.html',
   styleUrls: ['./bookings.component.css'],
 })
+
 export class BookingsComponent {
   constructor(private bookingService: BookingServiceService, public dialog: MatDialog) {
     this.showRooms();
