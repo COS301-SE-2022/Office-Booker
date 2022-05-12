239b64b869f8148a32cb629c1f504cacce0f6096
Grant Bursnall <gbursnall@gmail.com>
2022-05-12 11:51:53 +0200

    ✨ (menu component): Added menu component for use

--- apps/office-booker/src/app/app.module.ts
+++ apps/office-booker/src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MatGridListModule } from '@angular/material/grid-list'
+import { MatGridListModule } from '@angular/material/grid-list';
 import { BookingsComponent } from './bookings/bookings.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, BookingsComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MatGridListModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    BookingsComponent,
+    MenuBarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
