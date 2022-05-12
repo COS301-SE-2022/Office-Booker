4675849b08961f728b63921581397ffcd6ab385c
brettdup <u19037717@tuks.co.za>
2022-05-12 08:39:11 +0200

    setup of menubar component

---
+++ apps/office-booker/src/app/menubar/menubar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'office-booker-menubar',
+  templateUrl: './menubar.component.html',
+  styleUrls: ['./menubar.component.css'],
+})
+export class MenubarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
