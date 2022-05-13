e4d13df983a91b579e1ecb1b9081fdf49811fc92
Yinghao Li <u20460687@tuks.co.za>
2022-05-13 03:36:43 +0200

    Added unit testing for main booking component
    Limited to functions that dont make use of the booking service

--- apps/office-booker/src/app/bookings/bookings.component.spec.ts
+++ apps/office-booker/src/app/bookings/bookings.component.spec.ts
@@ -3,10 +3,13 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { BookingsComponent } from './bookings.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { MatGridListModule } from '@angular/material/grid-list';
+//import { BookingServiceService } from '../services/booking-service.service';
+import { BookingServiceService, Room, Desk } from '../services/booking-service.service';
 
 describe('BookingsComponent', () => {
   let component: BookingsComponent;
   let fixture: ComponentFixture<BookingsComponent>;
+  let service: BookingServiceService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,4 +27,412 @@ describe('BookingsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should populate grid', () => {
+    jest.spyOn(component, "generateGrid");
+    component.generateGrid();
+    for (let i = 0; i < 12; i++) {
+      //component.deskGrid.push([]);
+      //component.bookedGrid.push([]);
+      //component.bookingIDGrid.push([]);
+      for (let j = 0; j < 12; j++) {
+        expect(component.deskGrid[i][j]).toEqual(false);
+        //component.deskGrid[i].push(false);
+        expect(component.bookedGrid[i][j]).toEqual(false);
+        //component.bookedGrid[i].push(false);
+        expect(component.bookingIDGrid[i][j]).toEqual(-1);
+        //component.bookingIDGrid[i].push(-1);
+      }
+    }
+  });
+
+  it('should return -1 when no desk at location', () => {
+    jest.spyOn(component, "getDeskIdAtLocation");
+    expect(component.getDeskIdAtLocation(5, 5)).toEqual(-1);
+  });
+
+  it('should return desk when desk at location', () => {
+    jest.spyOn(component, "getDeskIdAtLocation");
+    let desk: Desk = {
+      roomId: 1,
+      id: 2,
+      LocationRow: 5,
+      LocationCol: 5    
+    }
+    component.desks[0] = desk;
+    component.desks[1] = desk;
+    expect(component.getDeskIdAtLocation(5, 5)).toEqual(2);
+  });
+
+  it('should mark correct coords in bookedGrid', () => {
+    jest.spyOn(component, "markBooking");
+    let desk: Desk = {
+      roomId: 1,
+      id: 2,
+      LocationRow: 3,
+      LocationCol: 5    
+    }
+    let desk2: Desk = {
+      roomId: 1,
+      id: 1,
+      LocationRow: 2,
+      LocationCol: 2    
+    }
+    let desk3: Desk = {
+      roomId: 1,
+      id: 3,
+      LocationRow: 1,
+      LocationCol: 4    
+    }
+    let desk4: Desk = {
+      roomId: 1,
+      id: 4,
+      LocationRow: 8,
+      LocationCol: 9    
+    }
+
+    for (let i = 0; i < 12; i++) {
+      component.deskGrid.push([]);
+      component.bookedGrid.push([]);
+      component.bookingIDGrid.push([]);
+      for (let j = 0; j < 12; j++) {
+        component.deskGrid[i].push(false);
+        component.bookedGrid[i].push(false);
+        component.bookingIDGrid[i].push(-1);
+      }
+    }
+
+    component.desks[0] = desk;
+    component.desks[1] = desk2;
+    component.desks[2] = desk3;
+    component.desks[3] = desk4;
+    component.markBooking(3);
+    expect(component.bookedGrid[1][4]).toEqual(true);
+  });
+
+  it('should add correct ID in bookingIDGrid', () => {
+    jest.spyOn(component, "addBooking");
+    let desk: Desk = {
+      roomId: 1,
+      id: 2,
+      LocationRow: 3,
+      LocationCol: 5    
+    }
+    let desk2: Desk = {
+      roomId: 1,
+      id: 1,
+      LocationRow: 2,
+      LocationCol: 2    
+    }
+    let desk3: Desk = {
+      roomId: 1,
+      id: 3,
+      LocationRow: 1,
+      LocationCol: 4    
+    }
+    let desk4: Desk = {
+      roomId: 1,
+      id: 4,
+      LocationRow: 8,
+      LocationCol: 9    
+    }
+
+    for (let i = 0; i < 12; i++) {
+      component.deskGrid.push([]);
+      component.bookedGrid.push([]);
+      component.bookingIDGrid.push([]);
+      for (let j = 0; j < 12; j++) {
+        component.deskGrid[i].push(false);
+        component.bookedGrid[i].push(false);
+        component.bookingIDGrid[i].push(-1);
+      }
+    }
+
+    component.desks[0] = desk;
+    component.desks[1] = desk2;
+    component.desks[2] = desk3;
+    component.desks[3] = desk4;
+    component.addBooking(3, 4);
+    expect(component.bookingIDGrid[1][4]).toEqual(4);
+  });
+
+  it('should remove correct ID from bookingIDGrid', () => {
+    jest.spyOn(component, "removeBooking");
+    let desk: Desk = {
+      roomId: 1,
+      id: 2,
+      LocationRow: 3,
+      LocationCol: 5    
+    }
+    let desk2: Desk = {
+      roomId: 1,
+      id: 1,
+      LocationRow: 2,
+      LocationCol: 2    
+    }
+    let desk3: Desk = {
+      roomId: 1,
+      id: 3,
+      LocationRow: 1,
+      LocationCol: 4    
+    }
+    let desk4: Desk = {
+      roomId: 1,
+      id: 4,
+      LocationRow: 8,
+      LocationCol: 9    
+    }
+
+    for (let i = 0; i < 12; i++) {
+      component.deskGrid.push([]);
+      component.bookedGrid.push([]);
+      component.bookingIDGrid.push([]);
+      for (let j = 0; j < 12; j++) {
+        component.deskGrid[i].push(false);
+        component.bookedGrid[i].push(false);
+        component.bookingIDGrid[i].push(-1);
+      }
+    }
+
+    component.desks[0] = desk;
+    component.desks[1] = desk2;
+    component.desks[2] = desk3;
+    component.desks[3] = desk4;
+    component.removeBooking(3);
+    expect(component.bookingIDGrid[1][4]).toEqual(-1);
+  });
+
+  it('should get correct bookingID', () => {
+    jest.spyOn(component, "getBookingID");
+    let desk: Desk = {
+      roomId: 1,
+      id: 2,
+      LocationRow: 3,
+      LocationCol: 5    
+    }
+    let desk2: Desk = {
+      roomId: 1,
+      id: 1,
+      LocationRow: 2,
+      LocationCol: 2    
+    }
+    let desk3: Desk = {
+      roomId: 1,
+      id: 3,
+      LocationRow: 1,
+      LocationCol: 4    
+    }
+    let desk4: Desk = {
+      roomId: 1,
+      id: 4,
+      LocationRow: 8,
+      LocationCol: 9    
+    }
+
+    for (let i = 0; i < 12; i++) {
+      component.deskGrid.push([]);
+      component.bookedGrid.push([]);
+      component.bookingIDGrid.push([]);
+      for (let j = 0; j < 12; j++) {
+        component.deskGrid[i].push(false);
+        component.bookedGrid[i].push(false);
+        component.bookingIDGrid[i].push(-1);
+      }
+    }
+
+    component.desks[0] = desk;
+    component.desks[1] = desk2;
+    component.desks[2] = desk3;
+    component.desks[3] = desk4;
+    component.markBooking(3);
+    component.addBooking(3, 4);
+    expect(component.getBookingID(3)).toEqual(4);
+  });
+
+  it('should get return -1 if desk is not booked', () => {
+    jest.spyOn(component, "getBookingID");
+    let desk: Desk = {
+      roomId: 1,
+      id: 2,
+      LocationRow: 3,
+      LocationCol: 5    
+    }
+    let desk2: Desk = {
+      roomId: 1,
+      id: 1,
+      LocationRow: 2,
+      LocationCol: 2    
+    }
+    let desk3: Desk = {
+      roomId: 1,
+      id: 3,
+      LocationRow: 1,
+      LocationCol: 4    
+    }
+    let desk4: Desk = {
+      roomId: 1,
+      id: 4,
+      LocationRow: 8,
+      LocationCol: 9    
+    }
+
+    for (let i = 0; i < 12; i++) {
+      component.deskGrid.push([]);
+      component.bookedGrid.push([]);
+      component.bookingIDGrid.push([]);
+      for (let j = 0; j < 12; j++) {
+        component.deskGrid[i].push(false);
+        component.bookedGrid[i].push(false);
+        component.bookingIDGrid[i].push(-1);
+      }
+    }
+
+    component.desks[0] = desk;
+    component.desks[1] = desk2;
+    component.desks[2] = desk3;
+    component.desks[3] = desk4;
+    component.markBooking(3);
+    component.addBooking(3, 4);
+    expect(component.getBookingID(2)).toEqual(-1);
+  });
+
+  it('shouldunmark the correct grid', () => {
+    jest.spyOn(component, "unMarkBooking");
+    let desk: Desk = {
+      roomId: 1,
+      id: 2,
+      LocationRow: 3,
+      LocationCol: 5    
+    }
+    let desk2: Desk = {
+      roomId: 1,
+      id: 1,
+      LocationRow: 2,
+      LocationCol: 2    
+    }
+    let desk3: Desk = {
+      roomId: 1,
+      id: 3,
+      LocationRow: 1,
+      LocationCol: 4    
+    }
+    let desk4: Desk = {
+      roomId: 1,
+      id: 4,
+      LocationRow: 8,
+      LocationCol: 9    
+    }
+
+    for (let i = 0; i < 12; i++) {
+      component.deskGrid.push([]);
+      component.bookedGrid.push([]);
+      component.bookingIDGrid.push([]);
+      for (let j = 0; j < 12; j++) {
+        component.deskGrid[i].push(false);
+        component.bookedGrid[i].push(false);
+        component.bookingIDGrid[i].push(-1);
+      }
+    }
+
+    component.desks[0] = desk;
+    component.desks[1] = desk2;
+    component.desks[2] = desk3;
+    component.desks[3] = desk4;
+    component.markBooking(3);
+    component.unMarkBooking(3);
+    expect(component.bookedGrid[1][4]).toEqual(false);
+  });
+
+  it('should return true if booked', () => {
+    jest.spyOn(component, "isBooked");
+    let desk: Desk = {
+      roomId: 1,
+      id: 2,
+      LocationRow: 3,
+      LocationCol: 5    
+    }
+    let desk2: Desk = {
+      roomId: 1,
+      id: 1,
+      LocationRow: 2,
+      LocationCol: 2    
+    }
+    let desk3: Desk = {
+      roomId: 1,
+      id: 3,
+      LocationRow: 1,
+      LocationCol: 4    
+    }
+    let desk4: Desk = {
+      roomId: 1,
+      id: 4,
+      LocationRow: 8,
+      LocationCol: 9    
+    }
+
+    for (let i = 0; i < 12; i++) {
+      component.deskGrid.push([]);
+      component.bookedGrid.push([]);
+      component.bookingIDGrid.push([]);
+      for (let j = 0; j < 12; j++) {
+        component.deskGrid[i].push(false);
+        component.bookedGrid[i].push(false);
+        component.bookingIDGrid[i].push(-1);
+      }
+    }
+
+    component.desks[0] = desk;
+    component.desks[1] = desk2;
+    component.desks[2] = desk3;
+    component.desks[3] = desk4;
+    component.markBooking(3);
+    component.addBooking(3, 4)
+    expect(component.isBooked(3)).toEqual(true);
+  });
+
+  it('should return false if not booked', () => {
+    jest.spyOn(component, "isBooked");
+    let desk: Desk = {
+      roomId: 1,
+      id: 2,
+      LocationRow: 3,
+      LocationCol: 5    
+    }
+    let desk2: Desk = {
+      roomId: 1,
+      id: 1,
+      LocationRow: 2,
+      LocationCol: 2    
+    }
+    let desk3: Desk = {
+      roomId: 1,
+      id: 3,
+      LocationRow: 1,
+      LocationCol: 4    
+    }
+    let desk4: Desk = {
+      roomId: 1,
+      id: 4,
+      LocationRow: 8,
+      LocationCol: 9    
+    }
+
+    for (let i = 0; i < 12; i++) {
+      component.deskGrid.push([]);
+      component.bookedGrid.push([]);
+      component.bookingIDGrid.push([]);
+      for (let j = 0; j < 12; j++) {
+        component.deskGrid[i].push(false);
+        component.bookedGrid[i].push(false);
+        component.bookingIDGrid[i].push(-1);
+      }
+    }
+
+    component.desks[0] = desk;
+    component.desks[1] = desk2;
+    component.desks[2] = desk3;
+    component.desks[3] = desk4;
+    expect(component.isBooked(3)).toEqual(false);
+  });
+
 });
