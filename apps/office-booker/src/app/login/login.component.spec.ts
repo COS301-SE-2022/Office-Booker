22b291de6bbcffd356bb33ac627d84cdaabd6859
Brett du Plessis <u19037717@tuks.co.za>
2022-05-13 17:10:05 +0200

    Update login.component.spec.ts

--- apps/office-booker/src/app/login/login.component.spec.ts
+++ apps/office-booker/src/app/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing'; 
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
