19747c8f00a070412dd8906ad36c10222191f75e
Yinghao Li <u20460687@tuks.co.za>
2022-05-28 13:43:16 +0200

    Removed need for db

--- libs/api/desks/repository/data-access/src/lib/api-desks-repository-data-access.service.spec.ts
+++ libs/api/desks/repository/data-access/src/lib/api-desks-repository-data-access.service.spec.ts
@@ -19,7 +19,7 @@ describe('ApiDesksRepositoryDataAccessService', () => {
 
   describe('getDesks', () => {
     it('should return an array of desks', async () => {
-      prisma.booking.findMany = jest.fn().mockReturnValue([{ "LocationCol": 2, "LocationRow": 2, "id": 2, "roomId": 1 }, { "LocationCol": 1, "LocationRow": 1, "id": 1, "roomId": 1 }]);
+      prisma.desk.findMany = jest.fn().mockReturnValue([{ "LocationCol": 2, "LocationRow": 2, "id": 2, "roomId": 1 }, { "LocationCol": 1, "LocationRow": 1, "id": 1, "roomId": 1 }]);
       expect(await (await service.getDesks()).length).toBeGreaterThan(0);
       expect(await service.getDesks()).toEqual([{ "LocationCol": 2, "LocationRow": 2, "id": 2, "roomId": 1 }, { "LocationCol": 1, "LocationRow": 1, "id": 1, "roomId": 1 }]);
     });
@@ -27,7 +27,7 @@ describe('ApiDesksRepositoryDataAccessService', () => {
 
   describe('getDesksInRoom', () => {
     it('should return an array of desks in a room', async () => {
-      prisma.booking.findMany = jest.fn().mockReturnValue([{ "LocationCol": 2, "LocationRow": 2, "id": 2, "roomId": 1 }, { "LocationCol": 1, "LocationRow": 1, "id": 1, "roomId": 1 }, { "LocationCol": 2, "LocationRow": 2, "id": 3, "roomId": 5 }]);
+      prisma.desk.findMany = jest.fn().mockReturnValue([{ "LocationCol": 2, "LocationRow": 2, "id": 2, "roomId": 1 }, { "LocationCol": 1, "LocationRow": 1, "id": 1, "roomId": 1 }]);
       expect(await (await service.getDesksInRoom(1)).length).toBeGreaterThan(0);
       expect(await service.getDesksInRoom(1)).toEqual([{ "LocationCol": 2, "LocationRow": 2, "id": 2, "roomId": 1 }, { "LocationCol": 1, "LocationRow": 1, "id": 1, "roomId": 1 }]);
       console.log(await service.getDesksInRoom(1));
