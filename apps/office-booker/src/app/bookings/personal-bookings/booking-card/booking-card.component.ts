3a83e2ced7c8f15ac7b817f946b9503c1ffaaed2
Grant Bursnall <gbursnall@gmail.com>
2022-05-29 16:11:16 +0200

    Update booking-card.component.ts
    still to pass tests

--- apps/office-booker/src/app/bookings/personal-bookings/booking-card/booking-card.component.ts
+++ apps/office-booker/src/app/bookings/personal-bookings/booking-card/booking-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'office-booker-booking-card',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booking-card.component.css'],
 })
 export class BookingCardComponent{
-  constructor() {}
+  constructor() {
+  //
+  }
 
-  //ngOnInit(): void {}
 }
