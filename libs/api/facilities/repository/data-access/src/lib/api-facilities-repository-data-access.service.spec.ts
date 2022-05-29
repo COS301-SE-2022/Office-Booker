b40c6fd96e7e5a783b74197278ef0167f4244c9b
Yinghao Li <u20460687@tuks.co.za>
2022-05-29 13:05:57 +0200

    Unit test for ApiFacilitiesRepositoryDataAccessService

--- libs/api/facilities/repository/data-access/src/lib/api-facilities-repository-data-access.service.spec.ts
+++ libs/api/facilities/repository/data-access/src/lib/api-facilities-repository-data-access.service.spec.ts
@@ -1,8 +1,89 @@
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { ApiFacilitiesRepositoryDataAccessService } from './api-facilities-repository-data-access.service';
 import { PrismaService } from '@office-booker/api/shared/services/prisma/data-access';
+import { Module, NotFoundException } from '@nestjs/common';
+import exp = require('constants');
+import * as crypto from 'crypto';
 
 describe('ApiFacilitiesRepositoryDataAccessService', () => {
+  let service: ApiFacilitiesRepositoryDataAccessService;
+  let prisma;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [ApiFacilitiesRepositoryDataAccessService, PrismaService],
+    }).compile();
+    service = await module.get<ApiFacilitiesRepositoryDataAccessService>(ApiFacilitiesRepositoryDataAccessService);
+    prisma = await module.get<PrismaService>(PrismaService);
+  });
+
+  describe('getFacilitiesForDesk', () => {
+    it('should return an array of facilities', async () => {
+      prisma.facility.findMany = jest.fn().mockReturnValue([{ id: 1, name: 'plug', count: 2, Desk: null, deskId: 2 }, { id: 2, name: 'ethernet', count: 1, Desk: null, deskId: 2 }, { id: 3, name: 'usb port', count: 1, Desk: null, deskId: 2 }]);
+      expect(await (await service.getFacilitiesForDesk(2)).length).toBeGreaterThan(0);
+      expect(await service.getFacilitiesForDesk(2)).toEqual([{ id: 1, name: 'plug', count: 2, Desk: null, deskId: 2 }, { id: 2, name: 'ethernet', count: 1, Desk: null, deskId: 2 }, { id: 3, name: 'usb port', count: 1, Desk: null, deskId: 2 }]);
+      expect(prisma.facility.findMany).toHaveBeenCalledWithObjectMatchingHash('248ed3d764bce50188fee61cfe1d8b12');
+    });
+  });
+});
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace jest {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface Matchers<R> {
+      toHaveBeenCalledWithObjectMatchingHash(expected: string): CustomMatcherResult;
+    }
+  }
+}
+
+expect.extend({
+  toHaveBeenCalledWithObjectMatchingHash(received, expected) {
+    const isSpy = (received: any) =>
+      received != null &&
+      received.calls != null &&
+      typeof received.calls.all === 'function' &&
+      typeof received.calls.count === 'function';
+
+    const receivedIsSpy = isSpy(received);
+
+    const calls = receivedIsSpy
+      ? received.calls.all().map((x: any) => x.args)
+      : received.mock.calls;
+
+    if (calls.length === 0) {
+      return {
+        pass: false,
+        message: () => `expected the function to be called with an object that hashes to '${expected}'. Instead, the function was not called.`,
+      };
+    }
+
+    if (calls[0].length === 0) {
+      return {
+        pass: false,
+        message: () => `expected the function to be called with an object that hashes to '${expected}'. Instead, the function was called, but not with any arguments.`,
+      };
+    }
+
+    const md5Hash = crypto.createHash('md5');
+    const receivedHash = md5Hash.update(JSON.stringify(calls[0][0])).digest('hex');
+    const pass = receivedHash === expected;
+
+    if (pass) {
+      return {
+        pass: true,
+        message: () => `expected the function to not be called with an object that hashes to '${expected}'. Instead, the passed object hashes to the same value.`,
+      };
+    } else {
+      return {
+        pass: false,
+        message: () => `expected the function to be called with an object that hashes to '${expected}'. Instead, the passed object hashes to '${receivedHash}'.`,
+      };
+    }
+  }
+});
+
+/*describe('ApiFacilitiesRepositoryDataAccessService', () => {
   let service: ApiFacilitiesRepositoryDataAccessService;
 
   beforeEach(async () => {
@@ -16,4 +97,4 @@ describe('ApiFacilitiesRepositoryDataAccessService', () => {
   it('should be defined', () => {
     expect(service).toBeTruthy();
   });
-});
+});*/
