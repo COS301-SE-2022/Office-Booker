23d76b1808c8eb63037b72655294feae978363eb
brettdup <u19037717@tuks.co.za>
2022-05-29 19:11:14 +0200

    added cognito details to environment

--- apps/office-booker/src/environments/environment.prod.ts
+++ apps/office-booker/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true,
 };
+
+export const cognito = { 
+  userPoolId: 'us-east-1_26Kuy4Sgb',
+  userPoolWebClientId: '4njope4fv0qg2shjcr799qvdh9',
+}
--- apps/office-booker/src/environments/environment.ts
+++ apps/office-booker/src/environments/environment.ts
@@ -6,6 +6,11 @@ export const environment = {
   production: false,
 };
 
+export const cognito = { 
+  userPoolId: 'us-east-1_26Kuy4Sgb',
+  userPoolWebClientId: '4njope4fv0qg2shjcr799qvdh9',
+}
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
