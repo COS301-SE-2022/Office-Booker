c4b4e4418e34eff9b11f9e932c735971b37d82a6
u20538945 <u20538945@tuks.co.za>
2022-05-29 16:25:31 +0200

    mock data for the manage bookings
    This contains mock data for the manage bookings to see how the different displays work

--- apps/office-booker/src/app/bookings/personal-bookings/personal-bookings.component.ts
+++ apps/office-booker/src/app/bookings/personal-bookings/personal-bookings.component.ts
@@ -7,8 +7,40 @@ import { Router } from '@angular/router';
   styleUrls: ['./personal-bookings.component.css'],
 })
 export class PersonalBookingsComponent{
+
+  userBookings: object[] = [];
+
   constructor(private router: Router,) {
-    //
+    this.userBookings = this.getBookings();
+    console.log(this.userBookings);
   }
+
+  getBookings(){
+    return [{
+      startDate: new Date(2018, 11, 24, 8, 33, 30, 0), 
+      endDate: new Date(2018, 11, 24, 10, 33, 30, 0),
+      roomName: "Room1",
+      employee: 'Bob',
+      desk: 'B2',
+    }, 
+
+    {
+      startDate: new Date(2018, 11, 24, 8, 33, 30, 0), 
+      endDate: new Date(2018, 11, 24, 12, 33, 30, 0),
+      roomName: "Room2",
+      employee: 'Bob',
+      desk: 'A2',
+    }, 
+
+    {
+      startDate: new Date(2018, 11, 24, 8, 33, 30, 0), 
+      endDate: new Date(2018, 11, 24, 9, 33, 30, 0),
+      roomName: "Room3",
+      employee: 'Bob',
+      desk: 'C3',
+    }, ]
+  }
+
+
 
 }
