40f21c6bb21574505d3d74650a17966769408b22
brettdup <u19037717@tuks.co.za>
2022-05-30 15:46:36 +0200

    Update login.component.spec.ts

--- apps/office-booker/src/app/authentication/login/login.component.spec.ts
+++ apps/office-booker/src/app/authentication/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { RouterTestingModule } from '@angular/router/testing'; 
+import { FormsModule } from '@angular/forms';
 
 
 describe('LoginComponent', () => {
@@ -10,7 +11,7 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, FormsModule],
     }).compileComponents();
   });
 
