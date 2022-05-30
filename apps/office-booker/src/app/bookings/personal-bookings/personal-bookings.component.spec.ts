a50352ef636a4f0c6c5c18a3f96e2221857ea65a
brettdup <u19037717@tuks.co.za>
2022-05-30 16:00:48 +0200

    fixing router testing module imports

--- apps/office-booker/src/app/bookings/personal-bookings/personal-bookings.component.spec.ts
+++ apps/office-booker/src/app/bookings/personal-bookings/personal-bookings.component.spec.ts
@@ -1,7 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PersonalBookingsComponent } from './personal-bookings.component';
-import { RouterTestingModule } from '@angular/router/testing'; 
+import { RouterTestingModule } from '@angular/router/testing'
 import { MatCardModule } from '@angular/material/card'
+import { BookingCardComponent } from './booking-card/booking-card.component';
+
+
 
 
 describe('PersonalBookingsComponent', () => {
