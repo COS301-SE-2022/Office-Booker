880313c669d3fabdfccd7c64a73e731b89d50f6b
brettdup <u19037717@tuks.co.za>
2022-06-02 17:45:49 +0200

    imported httpclientestingmodule

--- apps/office-booker/src/app/bookings/personal-bookings/personal-bookings.component.spec.ts
+++ apps/office-booker/src/app/bookings/personal-bookings/personal-bookings.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PersonalBookingsComponent } from './personal-bookings.component';
 import { RouterTestingModule } from '@angular/router/testing'
 import { MatCardModule } from '@angular/material/card'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+
 
 
 describe('PersonalBookingsComponent', () => {
@@ -11,7 +13,7 @@ describe('PersonalBookingsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PersonalBookingsComponent],
-      imports: [RouterTestingModule, MatCardModule]
+      imports: [RouterTestingModule, MatCardModule, HttpClientTestingModule]
     }).compileComponents();
   });
 
