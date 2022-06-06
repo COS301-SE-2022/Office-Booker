7cc3697e00a94c292cad50aa65889edcb1e71a47
Yinghao Li <u20460687@tuks.co.za>
2022-06-06 11:47:52 +0200

    Update api-companies-repository-data-access.service.spec.ts

--- libs/api/companies/repository/data-access/src/lib/api-companies-repository-data-access.service.spec.ts
+++ libs/api/companies/repository/data-access/src/lib/api-companies-repository-data-access.service.spec.ts
@@ -4,8 +4,56 @@ import { ApiCompaniesRepositoryDataAccessService } from './api-companies-reposit
 
 describe('ApiCompaniesRepositoryDataAccessService', () => {
   let service: ApiCompaniesRepositoryDataAccessService;
+  let prisma;
 
   beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [ApiCompaniesRepositoryDataAccessService, PrismaService],
+    }).compile();
+    service = module.get<ApiCompaniesRepositoryDataAccessService>(ApiCompaniesRepositoryDataAccessService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  describe('getCompanies', () => {
+    it('should return an array of companies', async () => {
+      const companies = [
+        {
+          id: 1,
+          name: 'Apple',
+          Rooms: null,
+          Employee: null,
+        },
+        {
+          id: 2,
+          name: 'Microsoft',
+          Rooms: null,
+          Employee: null,
+        },
+      ];
+      prisma.company.findMany = jest.fn().mockReturnValue(companies);
+      const result = await service.getCompanies();
+      expect(result).toBeDefined();
+      expect(result.length).toBeGreaterThan(0);
+      expect(result).toEqual(companies);
+    });
+  });
+
+  describe('getCompanyById', () => {
+    it('should return a company', async () => {
+      const company = {
+        id: 1,
+        name: 'Apple',
+        Rooms: null,
+        Employee: null,
+      };
+      prisma.company.findUnique = jest.fn().mockReturnValue(company);
+      const result = await service.getCompanyById(1);
+      expect(result).toBeDefined();
+      expect(result).toEqual(company);
+    });
+  });
+
+  /*beforeEach(async () => {
     const module = await Test.createTestingModule({
       providers: [ApiCompaniesRepositoryDataAccessService, PrismaService],
     }).compile();
@@ -15,5 +63,5 @@ describe('ApiCompaniesRepositoryDataAccessService', () => {
 
   it('should be defined', () => {
     expect(service).toBeTruthy();
-  });
+  });*/
 });
