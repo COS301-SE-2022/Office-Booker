b5f56706b33146d424812d07a78669032495b862
u20538945 <u20538945@tuks.co.za>
2022-06-06 19:43:22 +0200

    Template for the Admins bookings
    Full implementation being worked on

--- apps/office-booker/src/app/bookings/admin-bookings/admin-bookings.component.spec.ts
+++ apps/office-booker/src/app/bookings/admin-bookings/admin-bookings.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { RouterTestingModule } from '@angular/router/testing'
+import { MatCardModule } from '@angular/material/card'
 import { AdminBookingsComponent } from './admin-bookings.component';
 
 describe('AdminBookingsComponent', () => {
