a511d152ff7ec333c1a6d1f1bc2361a784f12be1
Arul Agrawal <me@arul.io>
2022-06-09 13:29:03 +0200

    Fix permissions service test

--- libs/api/permissions/src/lib/api-permissions.service.spec.ts
+++ libs/api/permissions/src/lib/api-permissions.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test } from '@nestjs/testing';
-import { ApiPermissionsService } from './api-permissions-.service';
+import { ApiCompaniesRepositoryDataAccessService } from '@office-booker/api/companies/repository/data-access';
+import { ApiDesksRepositoryDataAccessService } from '@office-booker/api/desks/repository/data-access';
+import { ApiRoomsRepositoryDataAccessService } from '@office-booker/api/rooms/repository/data-access';
+import { PrismaService } from '@office-booker/api/shared/services/prisma/data-access';
+import { ApiUsersRepositoryDataAccessService } from '@office-booker/api/users/repository/data-access';
+import { ApiPermissionsService } from './api-permissions.service';
 
 describe('ApiPermissionsService', () => {
   let service: ApiPermissionsService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [ApiPermissionsService],
+      providers: [ApiPermissionsService, ApiUsersRepositoryDataAccessService, ApiCompaniesRepositoryDataAccessService, ApiDesksRepositoryDataAccessService, ApiRoomsRepositoryDataAccessService, PrismaService],
     }).compile();
 
     service = module.get(ApiPermissionsService);
