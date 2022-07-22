d4c4a4af34dc36811d4bc58a7d851dbb927e1a62
Yinghao Li <u20460687@tuks.co.za>
2022-07-22 12:55:45 +0200

    Imported matformfield

--- apps/office-booker/src/app/bookings/personal-bookings/invite-dialog/invite-dialog.component.spec.ts
+++ apps/office-booker/src/app/bookings/personal-bookings/invite-dialog/invite-dialog.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { InviteDialogComponent } from './invite-dialog.component';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+import { MatFormField } from '@angular/material/form-field';
+
 
 describe('InviteDialogComponent', () => {
   let component: InviteDialogComponent;
@@ -11,7 +13,7 @@ describe('InviteDialogComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [InviteDialogComponent],
-      imports: [],
+      imports: [MatFormField],
       providers: [
         { provide: MatDialogRef, useValue: {}},
         { provide: MatDialog, useValue: {}},
