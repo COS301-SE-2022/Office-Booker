27df54fe7c1c0cfbbd0f37a84ff5151349aa6c25
Yinghao Li <u20460687@tuks.co.za>
2022-07-25 20:56:26 +0200

    Email now sends to the specified address instead of a hard coded one

--- libs/api/mail/src/lib/mail.service.ts
+++ libs/api/mail/src/lib/mail.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@nestjs/common';
 export class MailService {
   constructor(private mailerService: MailerService) {}
 
-  async sendUserConfirmation() {
+  async sendUserConfirmation(email: string) {
     const url = `http://localhost:4200`;
 
     await this.mailerService.sendMail({
