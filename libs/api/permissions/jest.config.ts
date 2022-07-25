0fb80427c0777c1980e97b0e5aa20edb102d19c4
Yinghao Li <u20460687@tuks.co.za>
2022-07-25 18:54:56 +0200

    Test Fixes

--- libs/api/permissions/jest.config.ts
+++ libs/api/permissions/jest.config.ts
@@ -1,15 +1,21 @@
 module.exports = {
-  displayName: 'api-permissions-',
+  displayName: 'api-mail',
   preset: '../../../jest.preset.ts',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  testEnvironment: 'node',
+  coverageDirectory: '../../../coverage/libs/api/mail',
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/api/permissions/',
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
 };
