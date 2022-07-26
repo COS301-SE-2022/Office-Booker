9bf2d28b273eab4edd2fe615d021a0e0106f6b54
Yinghao Li <u20460687@tuks.co.za>
2022-07-26 13:16:12 +0200

    Improvements to guest invites
    Improved text alignment in confirmation dailogue and improved email function to send the email to specified guest instead of a hardcoded address.

--- libs/api/mail/src/lib/mail.service.ts
+++ libs/api/mail/src/lib/mail.service.ts
@@ -5,11 +5,11 @@ import { Injectable } from '@nestjs/common';
 export class MailService {
   constructor(private mailerService: MailerService) {}
 
-  async sendUserConfirmation() {
+  async sendUserConfirmation(email: string) {
     const url = `http://localhost:4200`;
 
     await this.mailerService.sendMail({
-      to: 'u20460687@tuks.co.za',
+      to: email,
       from: '"Office Booker" <support@example.com>', // override default from
       subject: 'Welcome to Office Booker!',
       html: '<p>You have been invited to office booker</p><p>Click the link to join: </p><a href="www.youtube.com">Click</a>', 
