0d390bef810b770a3c7235ba976fa4b57585acbc
Arul Agrawal <me@arul.io>
2022-07-28 17:46:39 +0400

    add api urls to environment

--- apps/office-booker/src/environments/environment.prod.ts
+++ apps/office-booker/src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 
 
-export const environment = { 
+export const environment = {
   cognito: {
     userPoolId: 'us-east-1_B4cTzx2oi',
     userPoolWebClientId: '4fq13t0k4n7rrpuvjk6tua951c',
   },
+  production: true,
+  API_URL: 'http://backend:3333'
 }
--- apps/office-booker/src/environments/environment.ts
+++ apps/office-booker/src/environments/environment.ts
@@ -3,11 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 
-export const environment = { 
+export const environment = {
   cognito: {
     userPoolId: 'us-east-1_B4cTzx2oi',
     userPoolWebClientId: '4fq13t0k4n7rrpuvjk6tua951c',
   },
+  production: false,
+  API_URL: 'http://localhost:3333',
 }
 
 /*
