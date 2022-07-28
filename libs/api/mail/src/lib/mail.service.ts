f5f1a2ae8cca201e8f18af45d0f7d4db57578dbb
Yinghao Li <u20460687@tuks.co.za>
2022-07-28 18:20:02 +0200

    Changed format of the email sent to recipients

--- libs/api/mail/src/lib/mail.service.ts
+++ libs/api/mail/src/lib/mail.service.ts
@@ -12,7 +12,7 @@ export class MailService {
       to: email,
       from: '"Office Booker" <support@example.com>', // override default from
       subject: 'Welcome to Office Booker!',
-      html: '<p>You have been invited to office booker</p><p>Click the link to join: </p><a href="www.youtube.com">Click</a>', 
+      html: '<!DOCTYPE html><html><head><link href=\'https://fonts.googleapis.com/css?family=Maven Pro\' rel=\'stylesheet\'><style>body {font-family: \'Maven Pro\';font-size: 22px;}</style></head><body><h1>Office Booker</h1><p>You have been invited to join Office Booker as a guest, click the button below to register a guest account. If you did not request access, ignore this email.</p><a href="https://officebooker.co.za/guest-registration"><button type="button" class="button">Guest Registration</button></a><style>.button {border: none;color: white;padding: 15px 32px;text-align: center;text-decoration: none;display: inline-block;font-size: 16px;margin: 4px 2px;cursor: pointer;background-color: rgb(64,76,166);border-radius: 7px;}</style></body></html>', 
       context: { // ✏️ filling curly brackets with content
         name: 'Ying',
         url,
