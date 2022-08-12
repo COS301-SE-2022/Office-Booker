8805534e0988a82b091fc696f1a7995a8c492195
Brett du Plessis <duplessisbrett@icloud.com>
2022-08-12 08:46:45 +0200

    generated landing page

---
+++ apps/office-booker/src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'office-booker-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css'],
+})
+export class LandingPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
