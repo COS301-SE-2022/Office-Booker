5dbfcbb35c10ff03173b696c4219596108af4e37
Brett du Plessis <duplessisbrett@icloud.com>
2022-08-12 09:08:53 +0200

    import HttpTesting module

--- apps/office-booker/src/app/landing-page/landing-page.component.spec.ts
+++ apps/office-booker/src/app/landing-page/landing-page.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingPageComponent } from './landing-page.component';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+
+
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
   let fixture: ComponentFixture<LandingPageComponent>;
@@ -9,6 +12,7 @@ describe('LandingPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LandingPageComponent],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LandingPageComponent);
