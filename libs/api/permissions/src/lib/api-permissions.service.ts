90230712b4807b9b823154374f930ce089478c29
Arul Agrawal <me@arul.io>
2022-08-25 13:57:02 +0200

    make permissions service use a user object, which I should be able to obtain from somewhere for free?

--- libs/api/permissions/src/lib/api-permissions.service.ts
+++ libs/api/permissions/src/lib/api-permissions.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, Param } from '@nestjs/common';
+import { Injectable} from '@nestjs/common';
 import { ApiBookingsRepositoryDataAccessService } from '@office-booker/api/bookings/repository/data-access';
 import { ApiCompaniesRepositoryDataAccessService } from '@office-booker/api/companies/repository/data-access';
 import { ApiDesksRepositoryDataAccessService } from '@office-booker/api/desks/repository/data-access';
 import { ApiRoomsRepositoryDataAccessService } from '@office-booker/api/rooms/repository/data-access';
 import { ApiUsersRepositoryDataAccessService } from '@office-booker/api/users/repository/data-access';
+import { Employee } from '@prisma/client';
 
 @Injectable()
 export class ApiPermissionsService {
@@ -13,19 +14,18 @@ export class ApiPermissionsService {
         private companyService: ApiCompaniesRepositoryDataAccessService,
         private bookingService: ApiBookingsRepositoryDataAccessService) {}
 
-
-    async userAndDesk(@Param() userId: number, @Param() deskId: number) : Promise<boolean> {
-        const user = await this.userService.getUserById(userId);
-        const desk = await this.deskService.getDeskById(deskId);
-        const room = await this.roomService.getRoomById(desk.roomId);
-        const company = await this.companyService.getCompanyById(room.companyId);
-        // console.log("user and desk companies match!")
-        return user.companyId === company.id;
+    async userAndCompany(user: Employee, companyId: number) : Promise<boolean> {
+        return user.companyId == companyId;
     }
 
-    async userAndBooking(userId: number, bookingId: number) : Promise<boolean> {
-        const user = await this.userService.getUserById(userId);
+    async userAndDesk(user: Employee, deskId: number) : Promise<boolean> {
+        const desk = await this.deskService.getDeskById(deskId);
+        const room = await this.roomService.getRoomById(desk.roomId);
+        return this.userAndCompany(user, room.companyId);
+    }
+
+    async userAndBooking(user: Employee, bookingId: number) : Promise<boolean> {
         const booking = await this.bookingService.getBookingById(bookingId);
-        return this.userAndDesk(userId, booking.deskId) || user.admin;
+        return this.userAndDesk(user, booking.deskId) || user.admin;
     }
 }
