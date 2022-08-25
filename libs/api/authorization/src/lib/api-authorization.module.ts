43c9f8a9830414828496631c6817bd31287e4101
Arul Agrawal <me@arul.io>
2022-08-25 12:47:17 +0200

    add user service and dependencies to auth module

--- libs/api/authorization/src/lib/api-authorization.module.ts
+++ libs/api/authorization/src/lib/api-authorization.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthConfig } from './auth.config';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import { ApiUsersRepositoryDataAccessService } from '@office-booker/api/users/repository/data-access';
+import { PrismaService } from '@office-booker/api/shared/services/prisma/data-access';
 
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [],
-  providers: [AuthConfig, JwtStrategy],
+  providers: [AuthConfig, JwtStrategy, ApiUsersRepositoryDataAccessService, PrismaService],
   exports: [],
 })
 export class ApiAuthorizationModule {}
