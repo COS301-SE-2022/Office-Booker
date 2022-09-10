6c9f4857daed2c28be1c664ac23348fc4279a851
GrantBursnall <gbursnall@gmail.com>
2022-09-10 18:13:27 +0200

    ✨ (maker): start of saving map

--- apps/office-booker/src/app/office-maker/office-maker.component.ts
+++ apps/office-booker/src/app/office-maker/office-maker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BookingServiceService, Desk } from '../services/booking-service.service';
 import { SVGService } from '../services/svg.service';
 
 @Component({
@@ -8,8 +9,11 @@ import { SVGService } from '../services/svg.service';
   styleUrls: ['./office-maker.component.css'],
 })
 export class OfficeMakerComponent /*implements OnInit*/ {
-  idCounter = 0;
-  //constructor() {}
+  idCounterDesk = 0;
+  idCounterMeetingRoom = 0;
+
+  desks: Array<Desk> = [];
+  constructor(private bookingService: BookingServiceService) {}
 
   //ngOnInit(): void {}
 
@@ -21,8 +25,9 @@ export class OfficeMakerComponent /*implements OnInit*/ {
     newDesk.setAttribute("width", "65");
     newDesk.setAttribute("height", "35");
     newDesk.setAttribute("fill", "green");
-    newDesk.setAttribute("id", "desk-"+this.idCounter.toString());
-    this.idCounter++;
+    newDesk.setAttribute("isMeetingRoom", "false");
+    newDesk.setAttribute("id", "desk-"+this.idCounterDesk.toString());
+    this.idCounterDesk++;
 
     const svg = document.getElementById("create-desk");
     svg?.appendChild(newDesk);
@@ -36,10 +41,28 @@ export class OfficeMakerComponent /*implements OnInit*/ {
     newMeetingRoom.setAttribute("width", "150");
     newMeetingRoom.setAttribute("height", "150");
     newMeetingRoom.setAttribute("fill", "brown");
-    newMeetingRoom.setAttribute("id", "desk-"+this.idCounter.toString());
-    this.idCounter++;
+    newMeetingRoom.setAttribute("isMeetingRoom", "true");
+    newMeetingRoom.setAttribute("id", "desk-"+this.idCounterMeetingRoom.toString());
+    this.idCounterMeetingRoom++;
 
     const svg = document.getElementById("create-meetingRoom");
     svg?.appendChild(newMeetingRoom);
+  }
+
+  saveMap(){
+    const map = document.querySelectorAll("svg#dropzone");
+    map.forEach(node => {
+      const rects = node.children;
+      Array.from(rects).forEach(rect => {
+        const attrb = rect.attributes;
+        const newRect = {} as Desk;
+        newRect.LocationCol = Number(attrb.getNamedItem('x')?.value);
+        newRect.LocationRow = Number(attrb.getNamedItem('y')?.value);
+        newRect.Width = Number(attrb.getNamedItem('width')?.value);
+        newRect.Height = Number(attrb.getNamedItem('height')?.value);
+        newRect.isMeetingRoom = attrb.getNamedItem("isMeetingRoom")?.value ==='true';
+        console.log(newRect);
+      })
+    });
   }
 }
