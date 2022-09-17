fee038d104fa2a1c1f6e4af23d879a1472b75cdd
Arul Agrawal <me@arul.io>
2022-09-17 21:58:41 +0400

    create postdto for wall

--- libs/api/walls/api/src/lib/api-walls-api.controller.ts
+++ libs/api/walls/api/src/lib/api-walls-api.controller.ts
@@ -2,6 +2,14 @@ import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiWallsDataAccessService } from '@office-booker/api/walls/data-access';
 
+class createWallDto {
+    roomId: number;
+    Pos1X: number;
+    Pos1Y: number;
+    Pos2X: number;
+    Pos2Y: number;
+}
+
 @UseGuards(AuthGuard('jwt'))
 @Controller('walls')
 export class ApiWallsApiController {
@@ -10,5 +18,15 @@ export class ApiWallsApiController {
     @Get("/room/:roomId")
     async getWallsInRoom(@Param('roomId') roomId: string) {
         return await this.wallService.getWallsInRoom(Number(roomId));
+    }
+
+    @Get("/wall/:wallId")
+    async getWallsById(@Param('wallId') wallId: string) {
+        return await this.wallService.getWallbyId(Number(wallId));
+    }
+
+    @Post("/")
+    async createWall() {
+
     }
 }
