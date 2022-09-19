19855071111308691873d3a87466e5c5dc9771fd
GrantBursnall <gbursnall@gmail.com>
2022-09-19 16:03:49 +0200

    ✨ (delete): delete line

--- apps/office-booker/src/app/directives/drawable.directive.ts
+++ apps/office-booker/src/app/directives/drawable.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, OnInit, HostListener } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, HostListener, Output, EventEmitter } from '@angular/core';
 import { SVGService } from '../services/svg.service';
 
 @Directive({
@@ -9,11 +9,14 @@ import { SVGService } from '../services/svg.service';
 export class DrawableDirective {
   @Input() officeBookerDrawable = false;
   @Input() draw = false;
+  @Output() deleteLineId = new EventEmitter<string>();
 
   posX = 0;
   posY = 0;
   newWall: any;
   line: any;
+  idCounterWall = 0;
+  selectedItemId = "default";
 
   @HostListener('click', ['$event'])
   onclick(event: any) {
@@ -27,7 +30,8 @@ export class DrawableDirective {
         this.newWall.setAttribute("x2", "1");
         this.newWall.setAttribute("y2", "1");
         this.newWall.setAttribute("style", "stroke:rgb(0,0,0);stroke-width:5");
-        this.newWall.setAttribute("id", "wall-0");
+        this.newWall.setAttribute("id", "wall-"+this.idCounterWall.toString());
+        this.newWall.onclick = () => this.selectItem(this.newWall.id);
         //newWall.setAttribute("len", '1')
         //newWall.setAttribute("transform", "rotate(0)");
         dropzone.appendChild(this.newWall);
@@ -66,4 +70,10 @@ export class DrawableDirective {
   roundNum(n: number) {
     return Math.round(n / 10) * 10;
   }
+
+  selectItem(itemId: string) {
+    this.selectedItemId = itemId;
+    this.deleteLineId.emit(itemId);
+  }
+
 }
