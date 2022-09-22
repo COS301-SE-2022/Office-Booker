5823b8f30ea6c77e9a227d2a7607b1e111b2245f
Yinghao Li <u20460687@tuks.co.za>
2022-09-22 21:18:54 +0200

    Update api-desks-api.controller.spec.ts

--- libs/api/desks/api/src/lib/api-desks-api.controller.spec.ts
+++ libs/api/desks/api/src/lib/api-desks-api.controller.spec.ts
@@ -35,3 +35,48 @@ describe('ApiDesksApiController Unit Testing', () => {
     expect(service.getDesksInRoom).toHaveBeenCalledWith(Number(roomId));
   })
 });
+
+describe('ApiDesksApiController Integration Testing', () => {
+  let controller: ApiDesksApiController;
+  let service: ApiDesksRepositoryDataAccessService;
+
+  beforeAll(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [ApiDesksApiController],
+      providers: [ApiDesksRepositoryDataAccessService, PrismaService],
+    }).compile();
+    controller = app.get<ApiDesksApiController>(ApiDesksApiController);
+    service = app.get<ApiDesksRepositoryDataAccessService>(ApiDesksRepositoryDataAccessService);
+  });  
+
+  it("testing getDesks method", async () => {
+    const res = await controller.getAll();
+    expect(res.length).toBeGreaterThan(0);
+  })
+
+  it("calling getDeskByRoomId method", async () => {
+    const res = await controller.getDesksInRoom('3');
+    expect(res).toEqual([
+      {
+        id: 28,
+        roomId: 3,
+        LocationRow: 0,
+        LocationCol: 540,
+        Height: 200,
+        Width: 300,
+        isMeetingRoom: true,
+        capacity: 10
+      },
+      {
+        id: 29,
+        roomId: 3,
+        LocationRow: 210,
+        LocationCol: 0,
+        Height: 50,
+        Width: 30,
+        isMeetingRoom: false,
+        capacity: 15
+      }
+    ]);
+  })
+});
