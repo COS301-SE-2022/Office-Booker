b34429409967cc5abfd9123673924fc09b19dea8
Yinghao Li <u20460687@tuks.co.za>
2022-09-28 20:28:12 +0200

    Update api-walls-data-access.service.spec.ts

--- libs/api/walls/data-access/src/lib/api-walls-data-access.service.spec.ts
+++ libs/api/walls/data-access/src/lib/api-walls-data-access.service.spec.ts
@@ -1,27 +1,11 @@
 import { Test } from '@nestjs/testing';
 import { PrismaService } from '@office-booker/api/shared/services/prisma/data-access';
+import { prisma } from '@prisma/client';
 import { ApiWallsDataAccessService } from './api-walls-data-access.service';
 
-describe('ApiWallsDataAccessService', () => {
-  let service: ApiWallsDataAccessService;
-
-  beforeEach(async () => {
-    const module = await Test.createTestingModule({
-      providers: [ApiWallsDataAccessService, PrismaService],
-    }).compile();
-
-    service = module.get(ApiWallsDataAccessService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeTruthy();
-  });
-});
-
-describe('ApiWallsDataAccessService Integration Testing', () => {
+describe('ApiWallsDataAccessService Unit Testing', () => {
   let service: ApiWallsDataAccessService;
   let prisma;
-  let testCompany;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -30,7 +14,7 @@ describe('ApiWallsDataAccessService Integration Testing', () => {
 
     service = module.get<ApiWallsDataAccessService>(ApiWallsDataAccessService);
     prisma = module.get<PrismaService>(PrismaService);
-    
+
   });
 
   it('should get all walls in a room', async () => {
@@ -65,24 +49,41 @@ describe('ApiWallsDataAccessService Integration Testing', () => {
     expect(result).toEqual(walls);
   });
 
-  it ('should get a wall by id', async () => {
+  it('should get a wall by id', async () => {
+    prisma.wall.findUnique = jest.fn().mockReturnValue({
+      id: 1,
+      Pos1X: 3,
+      Pos1Y: 3,
+      Pos2X: 3,
+      Pos2Y: 3,
+      roomId: 1,
+    });
     const wall = await service.getWallbyId(1);
-    expect(wall).toBeDefined();
+    expect(wall).toEqual({
+      id: 1,
+      Pos1X: 3,
+      Pos1Y: 3,
+      Pos2X: 3,
+      Pos2Y: 3,
+      roomId: 1,
+    });
   });
-
-  it ('should create a wall', async () => {
-    const wall = await service.createWall(1, 0, 0, 1, 1);
-    expect(wall).toBeDefined();
-  });
-
-  it ('should delete a wall', async () => {
-    const wall = await service.createWall(1, 0, 0, 1, 1);
-    console.log(wall);
-    const deletedWall = await service.deleteWall(wall.id);
-    expect(deletedWall).toBeDefined();
-  });
-
-  
-
-
 });
+describe('ApiWallsDataAccessService Integration Testing', () => {
+  let service: ApiWallsDataAccessService;
+  let prisma;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [ApiWallsDataAccessService, PrismaService],
+    }).compile();
+
+    service = module.get<ApiWallsDataAccessService>(ApiWallsDataAccessService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeTruthy();
+  });
+});
+
