61358c539f94cea4ca01afa5cbc163139fd8fcb7
Arul Agrawal <me@arul.io>
2022-09-29 23:02:05 +0200

    remove commented out code

--- apps/office-booker/src/app/bookings/voting-bookings/voting-bookings.component.ts
+++ apps/office-booker/src/app/bookings/voting-bookings/voting-bookings.component.ts
@@ -26,21 +26,11 @@ export class VotingBookingsComponent {
   }
 
   ngOnInit() {
-    // this.getDesksByRoomId(1);
     this.getCurrentUser();
   }
 
   getScore(scoreToBeAdded: number, bookingId: number) {
-    // let i: number;
-    // for (i = 0; i < this.Users.length; i++) {
-    //   if (this.Users[i].id == employeeRate) {
-    //     this.rateUser.currentRating = this.Users[i].currentRating + score;
-    //     this.rateUser.ratingsReceived = this.Users[i].ratingsReceived + 1;
-    //     this.makeVote(employeeRate, this.rateUser);
-    //   }
-    // }
     this.makeVote(bookingId, scoreToBeAdded);
-
   }
 
   openSnackBar(message: number) {
@@ -60,65 +50,13 @@ export class VotingBookingsComponent {
     return true;
   }
 
-  // getUsers() { // this gets the users in the database for a specific company.
-  //   this.bookingService.getAllEmployees().subscribe(res => {
-  //     res.forEach(user => {
-  //       if (user.id != this.currentUser.id) { // this makes sure that the current user is not included so they can not rate themselves.
-  //         this.Users.push(user);
-  //         this.getBookings(user.id, user.name);
-  //       }
-  //     })
-  //   });
-  // }
-
-
-  // getDesksByRoomId(roomId: number) {
-  //   this.bookingService.getDesksByRoomId(roomId).subscribe(res => {
-  //     res.forEach(desk => {
-  //       const newDesk = {} as Desk;
-  //       newDesk.id = desk.id;
-  //       newDesk.LocationCol = desk.LocationCol;
-  //       newDesk.LocationRow = desk.LocationRow;
-  //       newDesk.roomId = desk.roomId;
-  //       newDesk.bookings = [];
-  //       this.getBookingsByDeskId(desk.id);
-
-  //       this.desks.push(newDesk);
-  //       //this.changeDetection.detectChanges();
-  //     });
-  //   })
-
-  // }
-
-  // getBookingsByDeskId(deskId: number) {
-  //   let bookingReturn = false;
-
-  //   this.bookingService.getBookingsByDeskId(this.currentUser.id).subscribe(res => {
-  //     res.forEach(booking => {
-  //       if (booking) {
-  //         bookingReturn = true;
-  //       }
-  //       for (let i = 0; i < this.desks.length; i++) {
-  //         if (this.desks[i].id == deskId) {
-  //           this.desks[i].booking = bookingReturn;
-  //           this.desks[i].bookings.push(booking);
-  //         }
-  //       }
-  //       //this.changeDetection.detectChanges();
-  //     });
-  //   })
-  // }
-
   getBookings(userId: number) {
     this.userBookings.splice(0);
     this.bookingService.getBookingsAllowedToVote(userId).subscribe(res => {
       res.forEach(booking => {
-        //console.log(booking);
         const compareDate = new Date();
         const bookingDate = new Date(booking.endsAt);
         bookingDate.setHours(bookingDate.getHours() - 2);
-        // console.log(compareDate);
-        // console.log(bookingDate);
 
         const sameDay = (d1: Date, d2: Date) => {
           return (
@@ -127,9 +65,7 @@ export class VotingBookingsComponent {
             d1.getDate() === d2.getDate()
           );
         };
-        // if (compareDate.setHours(0, 0, 0, 0) == bookingDate.setHours(0, 0, 0, 0)) {
         if (sameDay(compareDate, bookingDate)) {
-          // console.log("time verify passed!");
           const newBooking = {} as Booking;
           newBooking.id = booking.id;
           newBooking.deskId = booking.deskId;
@@ -138,11 +74,9 @@ export class VotingBookingsComponent {
           newBooking.employeeId = booking.employeeId;
           newBooking.employeeName = this.currentUser.name;
           this.userBookings.push(newBooking);
-          // console.log("all bookings:" + this.userBookings);
           this.changeDetection.detectChanges();
         }
       });
-      //this.changeDetection.detectChanges();
     })
   }
 
