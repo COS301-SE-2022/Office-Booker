d2a8d63cf1e6c14958572aade9cd6993e2de9af7
Yinghao Li <u20460687@tuks.co.za>
2022-09-29 15:31:56 +0200

    Update office-maker.service.spec.ts

--- apps/office-booker/src/app/services/office-maker.service.spec.ts
+++ apps/office-booker/src/app/services/office-maker.service.spec.ts
@@ -3,18 +3,50 @@ import { TestBed } from '@angular/core/testing';
 import { OfficeMakerService } from './office-maker.service';
 import { HttpClientModule } from '@angular/common/http';
 
-describe('OfficeMakerService', () => {
+describe('OfficeMakerService Unit Tests', () => {
   let service: OfficeMakerService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule], 
+      imports: [HttpClientModule],
       providers: [OfficeMakerService]
     });
     service = TestBed.inject(OfficeMakerService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should call createDesk method', () => {
+    service.createDesk = jest.fn();
+    service.createDesk(1, 1, 1, 1, 1, false, 1);
+    expect(service.createDesk).toHaveBeenCalledWith(1, 1, 1, 1, 1, false, 1);
+  });
+
+  it('should call getCompanies method', () => {
+    service.getCompanies = jest.fn();
+    service.getCompanies();
+    expect(service.getCompanies).toHaveBeenCalled();
+  });
+
+  it('should call createWall method', () => {
+    service.createWall = jest.fn();
+    service.createWall(1, 1, 1, 1, 1);
+    expect(service.createWall).toHaveBeenCalledWith(1, 1, 1, 1, 1);
+  });
+
+  it('should call getWallsByRoomId method', () => {
+    service.getWallsByRoomId = jest.fn();
+    service.getWallsByRoomId(5);
+    expect(service.getWallsByRoomId).toHaveBeenCalledWith(5);
+  });
+
+  it('should call deleteDeskbyId method', () => {
+    service.deleteDeskById = jest.fn();
+    service.deleteDeskById(3);
+    expect(service.deleteDeskById).toHaveBeenCalledWith(3);
+  });
+
+  it('should call deleteWallById method', () => {
+    service.deleteWallById = jest.fn();
+    service.deleteWallById(6);
+    expect(service.deleteWallById).toHaveBeenCalledWith(6);
   });
 });
