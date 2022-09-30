91fa6c476028fec2f001d47a81ffaa9cf1570e63
Yinghao Li <u20460687@tuks.co.za>
2022-09-30 02:49:21 +0200

    Tests for map bookings

--- apps/office-booker/src/app/bookings/map-bookings/map-bookings.component.spec.ts
+++ apps/office-booker/src/app/bookings/map-bookings/map-bookings.component.spec.ts
@@ -23,9 +23,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatLabel } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import * as exp from 'constants';
 
 
-describe('BookingsComponent', () => {
+describe('BookingsComponent Unit Tests', () => {
   let component: MapBookingsComponent;
   let fixture: ComponentFixture<MapBookingsComponent>;
 
@@ -52,7 +53,58 @@ describe('BookingsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should call printRooms method', () => {
+    component.getDesksByRoomId = jest.fn();
+    component.getWallsByRoomId = jest.fn();
+    component.printRooms(1);
+    expect(component.desks.length).toEqual(0);
+    expect(component.walls.length).toEqual(0);
+    expect(component.getDesksByRoomId).toHaveBeenLastCalledWith(1);
+    expect(component.getWallsByRoomId).toHaveBeenLastCalledWith(1);
+  });
+
+  it('should call changeOpen method', () => {
+    component.isOpen = false;
+    component.getFacilitiesForDesk = jest.fn();
+    component.changeOpen(5, true);
+    expect(component.getFacilitiesForDesk).toHaveBeenCalledWith(5);
+    expect(component.hoveredItemName).toEqual("Meeting Room 5");
+    expect(component.hoveredItemId).toEqual(5);
+    expect(component.hoveredItemType).toEqual("Meeting Room");
+  });
+
+  it('should call getFacilitiesForDesk method', () => {
+    component.getFacilitiesForDesk(0);
+    expect(component.numPlugs).toEqual(0);
+    expect(component.numMonitors).toEqual(0);
+    expect(component.numProjectors).toEqual(0);
+  });
+
+  it('should call selectToBook method', () => {
+    component.openDialog = jest.fn();
+    component.selectToBook(1, true);
+    expect(component.selectedItemBookings).toEqual([]);
+    expect(component.selected).toEqual(true);
+    expect(component.selectedItemName).toEqual("Meeting Room 1");
+    expect(component.selectedItemId).toEqual(1);
+    expect(component.selectedItemType).toEqual("Meeting Room");
+    expect(component.openDialog).toHaveBeenCalled();
+  });
+
+  it('should call deleteBooking method', () => {
+    component.deleteADeskBooking = jest.fn();
+    component.deleteBooking(5);
+    expect(component.deleteADeskBooking).toHaveBeenCalledWith(5);
+  });
+
+  it('should call filterBookings method', () => {
+    component.validateDate = jest.fn().mockReturnValue(true);
+    component.openFailSnackBar = jest.fn();
+    component.getBookingsByDeskId = jest.fn();
+    component.filterBookings();
+    expect(component.validateDate).toHaveBeenCalled();
+    component.validateDate = jest.fn().mockReturnValue(false);
+    component.filterBookings();
+    expect(component.openFailSnackBar).toHaveBeenCalled();
   });
 });
